1da81760b35d2a2a0b9d3ea2ccb9681ae336e4b5
SXIE-ai <80141385+SXIE-ai@users.noreply.github.com>
2025-12-29 22:36:08 +0800

    Update AppRaven.js script URL for response body

--- plugins/AppRaven/AppRaven.js
+++ plugins/AppRaven/AppRaven.js
@@ -6,7 +6,7 @@ AppRaven
 [rewrite_local]
 
 
-https://appraven.net/appraven/graphql url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/AppRaven.js
+https://appraven.net/appraven/graphql url script-response-body https://raw.githubusercontent.com/SXIE-ai/Loon/refs/heads/main/plugins/AppRaven/AppRaven.js
 [mitm] 
 
 hostname = appraven.net
